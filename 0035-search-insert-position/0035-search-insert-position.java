b561666d1c94403e423bbc760b871de9ed77354e
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-08-26 14:37:19 +0900

    Time: 0 ms (100%), Space: 44.9 MB (28.97%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,7 +2,16 @@ import java.util.*;
 
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int idx = Arrays.binarySearch(nums, target);
-        return idx >= 0 ? idx : -(idx + 1);
+        int left = 0;
+        int right = nums.length;
+
+        while(left < right){
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] < target) left = mid + 1;
+            else right = mid;
+        }
+
+        return left;
     }
 }
