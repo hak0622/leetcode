b0ccd1a1c489b14ce7d5558867000b60f602bb0a
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-08-27 13:09:08 +0900

    Time: 15 ms (11.49%), Space: 46.8 MB (82.5%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(int num : map.keySet()){
+            if(map.get(num) == 1) return num;
+        }
+        
+        return 0;
+    }
+}
