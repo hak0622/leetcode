60066315eab60aaf8ada9866b05756f110aa1b20
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-01 16:17:46 +0900

    Time: 13 ms (32.08%), Space: 52.4 MB (95.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,12 +3,13 @@ import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
         Map<Integer,Integer>map = new HashMap<>();
-        int line = nums.length / 2;
 
         for(int i=0; i<nums.length; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0) + 1);
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
 
-            if(map.get(nums[i]) > line) return nums[i];
+        for(int i=0; i<nums.length; i++){
+            if(map.get(nums[i]) > nums.length/2) return nums[i];
         }
         return 0;
     }
