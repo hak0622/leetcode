fc7166aaea9ff5ea8d3e5f5be2aee69182bf5ac9
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-01 16:33:46 +0900

    Time: 3 ms (57.24%), Space: 44.5 MB (47.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        if(s.equals(" ")) return true;
+
         StringBuilder sb = new StringBuilder();
-        
+        int idx = 0;
 
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
 
-            if(Character.isLetterOrDigit(c)){
-                sb.append(Character.toLowerCase(c));
+            if((c >='a' && c <='z') || (c>= 'A' && c<='Z') || (c>='0' && c<='9')){
+                if(c >= 'A' && c <='Z'){
+                    sb.append(Character.toLowerCase(c));
+                }else{
+                    sb.append(c);
+                }
             }
         }
-        int left = 0;
-        int right = sb.length()-1;
 
-        while(left < right){
-            if(sb.charAt(left) != sb.charAt(right)) return false;
+        for(int i=sb.length()-1; i>=0; i--){
+            char c = sb.charAt(i);
 
-            left++;
-            right--;
+            if(sb.charAt(idx) != c){
+                return false;
+            }
+            idx++;
         }
-
         return true;
     }
 }
