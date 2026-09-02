79295b4a2d99bbdbed2d14ec35ecc8289aa106dc
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-03 00:44:27 +0900

    Time: 3 ms (47.27%), Space: 46 MB (95.5%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+
+        if(prices[0] + prices[1] <= money){
+            return money - prices[0] - prices[1];
+        }
+        return money;
+    }
+}
