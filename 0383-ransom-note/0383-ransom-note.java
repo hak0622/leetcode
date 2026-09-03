c4314d87a34733db585ac9f7521b9df8e04128ff
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-03 15:56:33 +0900

    Time: 19 ms (7.94%), Space: 46.9 MB (34.71%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,26 +2,23 @@ import java.util.*;
 
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer>map1 = new HashMap<>();
-        HashMap<Character,Integer>map2 = new HashMap<>();
+        HashMap<Character,Integer>map = new HashMap<>();
 
         for(int i=0; i<ransomNote.length(); i++){
             char c = ransomNote.charAt(i);
-            map1.put(c, map1.getOrDefault(c,0)+1);
+            map.put(c, map.getOrDefault(c,0)+1);
         }
 
         for(int i=0; i<magazine.length(); i++){
             char c = magazine.charAt(i);
-
-            if(map1.containsKey(c)){
-                map1.put(c, map1.getOrDefault(c,0)-1);
+            if(map.containsKey(c)){
+                map.put(c, map.get(c)-1);
             }
         }
 
-        for(int count : map1.values()){
-            if(count >= 1) return false;
+        for(int v : map.values()){
+            if(v > 0) return false;
         }
-
         return true;
     }
 }
