9088bd248ee12c1894dc2ad492a11cf523558c0d
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-03 15:48:50 +0900

    Time: 0 ms (100%), Space: 43.1 MB (5.59%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -5,10 +5,8 @@ class Solution {
         HashMap<Character,String>map = new HashMap<>();
         String[]arr = s.split(" ");
 
-        if(pattern.length() != arr.length){
-            return false;
-        }
-        
+        if(pattern.length() != arr.length) return false;
+
         for(int i=0; i<pattern.length(); i++){
             char c = pattern.charAt(i);
 
@@ -20,7 +18,7 @@ class Solution {
                 if(map.containsValue(arr[i])){
                     return false;
                 }
-                map.put(c,arr[i]);
+                map.put(c, arr[i]);
             }
         }
         return true;
