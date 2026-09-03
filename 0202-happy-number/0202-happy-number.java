ee48a8716a003b120f6a9ac573c56cb07772fbba
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-03 15:15:58 +0900

    Time: 2 ms (14.59%), Space: 43 MB (5%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,21 +1,21 @@
+import java.util.*;
+
 class Solution {
     public boolean isHappy(int n) {
         HashSet<Integer>set = new HashSet<>();
 
         while(n != 1){
-            if(set.contains(n)) return false;
-
-            set.add(n);
-
             int sum = 0;
             String s = n + "";
+            char[] c = s.toCharArray();
 
-            for(int i=0; i<s.length(); i++){
-                char c = s.charAt(i);
-                int num = c - '0';
-
+            for(int i=0; i<c.length; i++){
+                int num = c[i] - '0';
                 sum = sum + num * num;
             }
+
+            if(set.contains(sum)) return false;
+            set.add(sum);
             n = sum;
         }
 
