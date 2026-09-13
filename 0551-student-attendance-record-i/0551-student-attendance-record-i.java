36b6cc78e777f3e67cc89d71672826322a7ba38e
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-13 18:19:23 +0900

    Time: 0 ms (100%), Space: 43 MB (22.84%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int countA = 0;
+        int countL = 0;
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c == 'A'){
+                countA++;
+                countL = 0;
+            }else if(c == 'L'){
+                countL++;
+            }else{
+                countL = 0;
+            }
+
+            if(countA >= 2 || countL >= 3) return false;
+        }
+
+        return true;
+    }
+}
