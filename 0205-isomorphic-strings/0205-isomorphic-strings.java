902a5061b1658888a3edd1ccf9234c1f89fec25b
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-15 18:25:07 +0900

    Time: 11 ms (64.83%), Space: 43.5 MB (93.56%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length() != t.length()) return false;
 
-        HashMap<Character,Character>map = new HashMap<>();
+        Map<Character,Character>map = new HashMap<>();
 
         for(int i=0; i<s.length(); i++){
             char c1 = s.charAt(i);
@@ -12,7 +12,7 @@ class Solution {
                 if(map.get(c1) != c2) return false;
             }else{
                 if(map.containsValue(c2)) return false;
-                map.put(c1,c2);
+                else map.put(c1,c2);
             }
         }
         return true;
