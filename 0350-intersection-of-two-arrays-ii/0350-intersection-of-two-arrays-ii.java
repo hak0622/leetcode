aa5bbb950fea58be230f1f095beac57d9cc9979b
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-15 23:59:35 +0900

    Time: 6 ms (32.29%), Space: 45.5 MB (9.3%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,22 +1,17 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer,Integer>map1 = new HashMap<>();
-        Map<Integer,Integer>map2 = new HashMap<>();
+        HashMap<Integer,Integer>map = new HashMap<>();
         List<Integer>list = new ArrayList<>();
 
         for(int i=0; i<nums1.length; i++){
-            map1.put(nums1[i], map1.getOrDefault(nums1[i],0)+1);
+            map.put(nums1[i], map.getOrDefault(nums1[i],0)+1);
         }
+        
         for(int i=0; i<nums2.length; i++){
-            map2.put(nums2[i], map2.getOrDefault(nums2[i],0)+1);
-        }
-
-        for(int k : map1.keySet()){
-            if(map2.containsKey(k)){
-                int count = Math.min(map1.get(k), map2.get(k));
-
-                for(int i=0; i<count; i++){
-                    list.add(k);
+            if(map.containsKey(nums2[i])){
+                if(map.get(nums2[i]) > 0){
+                    map.put(nums2[i], map.get(nums2[i]) - 1);
+                    list.add(nums2[i]);
                 }
             }
         }
