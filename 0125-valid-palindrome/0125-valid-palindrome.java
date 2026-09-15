18432a03825d2844308ce851ea31cf572304a75d
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-15 18:18:55 +0900

    Time: 3 ms (56.33%), Space: 44.5 MB (47.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
-        
+
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
-            
-            if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')){
+
+            if(!Character.isLetterOrDigit(c)){
+                continue;
+            }else{
                 sb.append(Character.toLowerCase(c));
             }
         }
-        int left = 0;
-        int right = sb.length()-1;
 
-        while(left < right){
-            if(sb.charAt(left) != sb.charAt(right)) return false;
-            left++;
-            right--;
+        for(int i=0; i<sb.length()/2; i++){
+            char c1 = sb.charAt(i);
+            char c2 = sb.charAt(sb.length() - i - 1);
+
+            if(c1 != c2){
+                return false;
+            }
         }
+
         return true;
     }
 }
