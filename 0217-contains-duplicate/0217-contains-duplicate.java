238a2de7467f4b944427ca75d332d4b32ff1c1ac
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-15 23:42:49 +0900

    Time: 18 ms (76.85%), Space: 108.2 MB (38.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,12 @@
-import java.util.*;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        HashSet<Integer>set = new HashSet<>();
 
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i] == nums[i+1]) return true;
+        for(int i=0; i<nums.length; i++){
+            if(set.contains(nums[i])) return true;
+            set.add(nums[i]);
         }
+
         return false;
     }
 }
