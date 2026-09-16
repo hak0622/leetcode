f66dc6b49c1a64e817827dcb43af92ba84e514ac
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-17 00:43:57 +0900

    Time: 13 ms (72.53%), Space: 46.5 MB (50.65%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -3,21 +3,21 @@ class Solution {
         int count = 0;
 
         for(int i=0; i<k; i++){
-            if(vowel(s.charAt(i))) count++;
+            if(check(s.charAt(i))) count++;
         }
 
         int max = count;
 
         for(int i=k; i<s.length(); i++){
-            if(vowel(s.charAt(i - k))) count--;
-            if(vowel(s.charAt(i))) count++;
+            if(check(s.charAt(i-k))) count--;
+            if(check(s.charAt(i))) count++;
 
             max = Math.max(max, count);
         }
         return max;
     }
 
-    public boolean vowel(char c){
+    public boolean check(char c){
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
