17cd9ed13bc69e8f32be3823757ab673ce0d906c
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-16 19:14:48 +0900

    Time: 2 ms (95.98%), Space: 48.5 MB (24.11%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -4,13 +4,9 @@ class Solution {
         int right = numbers.length - 1;
 
         while(left < right){
-            if(numbers[left] + numbers[right] > target){
-                right--;
-            }else if(numbers[left] + numbers[right] < target){
-                left++;
-            }else{
-                return new int[]{left+1,right+1};
-            }
+            if(numbers[left] + numbers[right] >target) right--;
+            else if(numbers[left] + numbers[right] < target) left++;
+            else return new int[]{left+1, right+1};
         }
         return new int[]{};
     }
