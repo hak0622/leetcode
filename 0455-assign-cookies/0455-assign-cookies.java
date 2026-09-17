f340205c5beb8e1fc586d73bd6192927e672592f
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-17 15:57:43 +0900

    Time: 13 ms (99.31%), Space: 51.6 MB (19.33%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,23 +1,20 @@
-import java.util.*;
-
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-
         int idxG = 0;
         int idxS = 0;
-        int count = 0;
+        int answer = 0;
 
         while(idxG < g.length && idxS < s.length){
             if(g[idxG] <= s[idxS]){
                 idxG++;
                 idxS++;
-                count++;
+                answer++;
             }else{
                 idxS++;
             }
         }
-        return count;
+        return answer;
     }
 }
