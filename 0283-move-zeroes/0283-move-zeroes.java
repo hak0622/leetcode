7c79862b6fd6189aed8741fc68c6c7a1bd4bfdb8
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-22 21:55:27 +0900

    Time: 2 ms (91.68%), Space: 48 MB (6.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int idx = 0;
+        int zero = 0;
 
         for(int i=0; i<nums.length; i++){
             if(nums[i] != 0){
-                int temp = nums[i];
-                nums[i] = nums[idx];
-                nums[idx] = temp;
-                idx++;
+                int temp = nums[zero];
+                nums[zero] = nums[i];
+                nums[i] = temp;
+                zero++;
             }
-        }
+        }        
     }
 }
