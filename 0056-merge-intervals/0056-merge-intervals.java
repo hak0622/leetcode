abacccc594d6e4d3fa7343d83b3abab3326d61e8
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-26 22:54:57 +0900

    Time: 8 ms (89.65%), Space: 49 MB (74.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,25 +1,30 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)-> a[0]-b[0]);
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+
         List<int[]>list = new ArrayList<>();
 
         int start = intervals[0][0];
         int end = intervals[0][1];
 
         for(int i=1; i<intervals.length; i++){
-            int nextStart = intervals[i][0];
-            int nextEnd = intervals[i][1];
-
-            if(nextStart <= end){
-                end = Math.max(end, nextEnd);
+            if(end >= intervals[i][0]){
+                end = Math.max(end, intervals[i][1]);
             }else{
                 list.add(new int[]{start,end});
-                start = nextStart;
-                end = nextEnd;
+
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
         }
+
         list.add(new int[]{start,end});
 
-        return list.toArray(new int[list.size()][]);
+        int[][]answer = new int[list.size()][2];
+
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
     }
 }
