927fd556895d2d91b63a8143ce3de79bc87c1f60
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-09-26 14:57:37 +0900

    Time: 6 ms (8.24%), Space: 44.4 MB (97.46%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,25 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        List<Integer>list = new ArrayList<>();
+        int[]answer = new int[nums1.length];
 
         for(int i=0; i<nums1.length; i++){
-            int find = nums1[i];
-            int answer = -1;
+            answer[i] = -1;
 
             for(int j=0; j<nums2.length; j++){
-                if(find == nums2[j]){
+                if(nums1[i] == nums2[j]){
                     for(int k=j+1; k<nums2.length; k++){
-                        if(nums2[k] > find){
-                            answer = nums2[k];
+                        if(nums2[j] < nums2[k]){
+                            answer[i] = nums2[k];
                             break;
                         }
                     }
-                    break;
                 }
             }
-            list.add(answer);
         }
-
-        return list.stream().mapToInt(i->i).toArray();
+        return answer;
     }
 }
